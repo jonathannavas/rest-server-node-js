3bea2140649c1dfdbe406bc4371484f408255c28
Jonathan <jonathan.navas9@hotmail.com>
2021-03-19 18:28:17 -0500

    Se implemento autenticacion a traves de google Sign In

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,9 @@ const { request, response } = require('express');
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = 'response') => {
 
@@ -12,6 +14,7 @@ const login = async ( req, res = 'response') => {
 
         //verificar si el correo existe
         const usuario = await Usuario.findOne({ correo });
+
         if(!usuario){
             return res.status(400).json({
                 msg: 'Usuario o contraseña incorrecto: email'
@@ -25,6 +28,7 @@ const login = async ( req, res = 'response') => {
         }
         //verificar la contraseña
         const passwordValido = bcryptjs.compareSync( password, usuario.password );
+
         if(!passwordValido){
             return res.status(400).json({
                 msg: 'Usuario o contraseña incorrecto: password'
@@ -50,6 +54,64 @@ const login = async ( req, res = 'response') => {
 
 }
 
+const googleSignIn = async ( req, res = response ) => {
+
+    const { id_token } = req.body;
+   
+    try {
+
+        const { nombre, correo, img } = await googleVerify( id_token );
+
+        //verificar si el correo ya existe
+        let usuario = await Usuario.findOne({ correo });
+ 
+        if( !usuario ){
+            //crar usuario en el campo de password enviar al menos 1 caracter de tipo string caso contrario salta el error
+
+            const data = {
+
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+
+            };
+
+            usuario = new Usuario( data ); 
+
+            await usuario.save();
+            
+        }
+
+
+        if( !usuario.estado ){
+
+            return res.status(401).json({
+                msg: 'Hable con el administrador - usuario bloqueado'
+            })
+
+        }
+
+        //generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de google no valido'
+        });
+
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = new Router();
@@ -14,6 +14,14 @@ router.post('/login', [
     validarCampos
     
 ], login);
+
+router.post('/google', [
+
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+
+    validarCampos
+    
+], googleSignIn);
 
 
 module.exports = router;
