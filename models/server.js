904cb5f599abef8fff497c5499631783bc5f9831
Jonathan <jonathan.navas9@hotmail.com>
2021-03-20 22:14:32 -0500

    Busquedas Inteligentes ya sea en productos, usuarios, categorias

--- models/server.js
+++ models/server.js
@@ -18,8 +18,8 @@ class Server {
             auth:       '/api/auth',
             categorias: '/api/categorias',
             usuarios:   '/api/usuarios',
-            productos:  '/api/productos'
-
+            productos:  '/api/productos',
+            buscar:     '/api/buscar'
         }
 
         //Conectar a la base de datos
@@ -53,6 +53,7 @@ class Server {
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.buscar, require('../routes/buscar'));
     
     }
 
